234d63d5e7e96eecab18a0f10315b03830ee9137
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-05 00:52:47 -0500

    Props en un componente de React

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TarjetaFruta = () => (
-  <div>
-    <h3>Titulo</h3>
-    <hr/>
-    <p>Descripción...</p>
-  </div>
-);
+const TarjetaFruta = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <hr/>
+      <p>{props.price}</p>
+    </div>
+  )
+};
 
 
 const App = () => (
   <div>
-    <TarjetaFruta/>
-    <TarjetaFruta></TarjetaFruta>
+    <TarjetaFruta name={'Manzana'} price={2.1} />
+    <TarjetaFruta name={'Naranja'} price={1.4} />
+    <TarjetaFruta name={'Pera'} price={1.7} />
   </div>
 );
 
