44a1feb6c4dcf68ef880ec56f8034d0d45d15455
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-05 00:43:01 -0500

    Crear componente funcional con React

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Jonathan Reátegui';
-
-const user = {
-  name: 'Jonthan Reátegui',
-  age: '30',
-  country: 'Venezuela'
-};
-
-function getInfo(userData) {
-  return `Hola React! ${userData.name} edad: ${userData.age}`
-}
-
-const App = <h1>{ getInfo(user) }</h1>;
-ReactDOM.render(App, document.getElementById('root'));
+const TarjetaFruta = () => (
+  <div>
+    <h3>Titulo</h3>
+    <hr/>
+    <p>Descripción...</p>
+  </div>
+);
 
 
+const App = () => (
+  <div>
+    <TarjetaFruta/>
+    <TarjetaFruta></TarjetaFruta>
+  </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
