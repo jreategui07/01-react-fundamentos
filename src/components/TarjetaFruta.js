16c7fa739855feb8a656ed65cfe59c8869b0b105
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-05 23:27:24 -0500

    Aplicar estilos CSS en línea a componente de React

--- src/components/TarjetaFruta.js
+++ src/components/TarjetaFruta.js
@@ -22,15 +22,35 @@ class TarjetaFruta extends React.Component {
     });
   }
   render() {
+
+    const hasItems = this.state.cantidad > 0;
+
+    const styles = {
+      fontFamily: 'Elvetica, Arial, Tahoma',
+      margin: '0 auto',
+      border: '1px solid black',
+      marginBottom: '1em',
+      borderRadius: '0.5em',
+      padding: '1em',
+      background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : '#FFF',
+      color: hasItems ? '#FFF' : '#000',
+      transition: 'all 400ms ease-out'
+    };
+
     return (
-      <div>
+      <div style={styles}>
         <h3>{this.props.name}</h3>
         <div>Cantidad: {this.state.cantidad}</div>
         <button onClick={this.agregar}>+</button>
         <button onClick={this.quitar}>-</button>
         <button onClick={this.limpiar}>Limpiar</button>
         <hr />
-        <p>{this.props.price}</p>
+        <p>
+          $ {this.props.price}
+        </p>
+        <p>
+          Total: {this.props.price * this.state.cantidad}
+        </p>
       </div>
     )
   }
