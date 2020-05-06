ccc07d0612bcbe4713dd3c13598f4e158e7d8d0b
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-06 00:03:14 -0500

    Aplicar estilos con clases CSS en componente de React

--- src/components/TarjetaFruta.js
+++ src/components/TarjetaFruta.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TarjetaFruta.css';
 
 class TarjetaFruta extends React.Component {
 
@@ -24,21 +25,11 @@ class TarjetaFruta extends React.Component {
   render() {
 
     const hasItems = this.state.cantidad > 0;
-
-    const styles = {
-      fontFamily: 'Elvetica, Arial, Tahoma',
-      margin: '0 auto',
-      border: '1px solid black',
-      marginBottom: '1em',
-      borderRadius: '0.5em',
-      padding: '1em',
-      background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : '#FFF',
-      color: hasItems ? '#FFF' : '#000',
-      transition: 'all 400ms ease-out'
-    };
+    const activeClass = `${hasItems ? 'TarjetaFruta-activa' : ''}`;
+    const classes = `TarjetaFruta ${activeClass}`;
 
     return (
-      <div style={styles}>
+      <div className={classes}>
         <h3>{this.props.name}</h3>
         <div>Cantidad: {this.state.cantidad}</div>
         <button onClick={this.agregar}>+</button>
