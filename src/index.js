526a066d6a675fd2df875e9a8549b4d9558517c8
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-05 22:02:23 -0500

    Inicializadores de propiedades para manejar eventos

--- src/index.js
+++ src/index.js
@@ -4,24 +4,48 @@ import ReactDOM from 'react-dom';
 class TarjetaFruta extends React.Component {
   constructor() {
     super();
+
+    /* this.agregar = this.agregar.bind(this);
+    this.quitar = this.quitar.bind(this); */
+
+    /* hacer bind de forma dinámica */
+    const METHODS = [
+      'agregar',
+      'quitar',
+      'limpiar'
+    ];
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
+    /* fin de hacer bind de forma dinámica */
+
     this.state = {
       cantidad: 0
     };
+  }
+  agregar() {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    });
+  }
+  quitar() {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    });
+  }
+  limpiar() {
+    this.setState({
+      cantidad: 0
+    });
   }
   render() {
     return (
       <div>
         <h3>{this.props.name}</h3>
         <div>Cantidad: {this.state.cantidad}</div>
-        <button
-          onClick={
-            () => {
-              this.setState({
-                cantidad: this.state.cantidad + 1
-              });
-            }
-          }
-        >Agregar</button>
+        <button onClick={this.agregar}>+</button>
+        <button onClick={this.quitar}>-</button>
+        <button onClick={this.limpiar}>Limpiar</button>
         <hr/>
         <p>{this.props.price}</p>
       </div>
