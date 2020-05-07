ac88ca6aaaf344e68c0c35d94bb2b29f77cd3ba8
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-06 22:46:34 -0500

    Usando el operador spread para pasar props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React, { Component } from 'react'
 // import TarjetaFruta from './components/TarjetaFruta';
 import Contador from './components/Contador';
 
-const App = () => (
-  <div>
-    {/* <TarjetaFruta name={'Manzana'} price={5} />
-    <TarjetaFruta name={'Naranja'} price={4} />
-    <TarjetaFruta name={'Pera'} price={3} /> */}
-    <Contador/>
-  </div>
-);
+import Gato from './components/Gato'
+
+class App extends Component {
+
+  state = {
+    fuerza: 100,
+    vidasRestantes: 7
+  }
+
+  render() {
+
+    const otrosDatos = {
+      raza: 'Tropical',
+      peleasNocturnas: 300
+    };
+
+    return (
+        <div>
+          {/* <TarjetaFruta name={'Manzana'} price={5} />
+          <TarjetaFruta name={'Naranja'} price={4} />
+          <TarjetaFruta name={'Pera'} price={3} /> */}
+          {/* <Contador/> */}
+          <Gato
+            name="Garfiled"
+            age="2 años"
+            {...otrosDatos}
+            {...this.state}
+          />
+        </div>
+    )
+  }
+}
 
 export default App;
---
+++ src/components/Gato/Gato.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Gato = (props) => (
+  <div>
+    <h1>Gato</h1>
+    <pre>
+      {JSON.stringify(props, null, 4)}
+    </pre>
+  </div>
+);
+
+export default Gato
